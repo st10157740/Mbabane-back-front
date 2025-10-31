ff1596dfd2e56ff62d6d6bd06868fa0c7f3afccc
Zainab Sulaman <st10157740@vcconnect.edu.za>
2025-10-31 20:33:03 +0200

    WIL

---
+++ MbabaneHighlandersBackend2/Model/Fixture.cs
@@ -0,0 +1,13 @@
+﻿namespace MbabaneHighlandersBackend2.Model
+{
+    public class Fixture
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly Time { get; set; }
+        public string HomeTeam { get; set; } = string.Empty;
+        public string AwayTeam { get; set; } = string.Empty;
+        public string? Stadium { get; set; }
+        public DateTime CreatedUtc { get; set; }
+    }
+}
